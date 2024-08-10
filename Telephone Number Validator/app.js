9c7c42a8c8e8cb1df71661223f75ce645cb089d5
Randi Febriadi <randifebriadi@gmail.com>
2024-08-10 19:47:55 +0700

    coba

--- Telephone Number Validator/app.js
+++ Telephone Number Validator/app.js
@@ -5,3 +5,11 @@ function telephoneCheck(str) {
 
 console.log(telephoneCheck("555-555-5555"));
 console.log(telephoneCheck("555-5555"));
+
+// function hpindo(str) {
+//   const rules = /^(\+62-|0)?(\d{3})[-\s]?(\d{4})[-\s]?(\d{4})$/;
+//   return rules.test(str);
+// }
+
+// console.log(hpindo("+62-853-9705-4275"));
+// console.log(hpindo("085397054275"));
